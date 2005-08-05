40d1c4b3f85ffd3d330aee2d87d925fc9348ae5a
weary <weary@gamehost.net>
2005-08-05 20:12:34 +0000

    [project @ added functions to more easily read database from c++]

--- piet_db.cpp
+++ piet_db.cpp
@@ -62,12 +62,11 @@ PyObject * piet_db_query(PyObject *self, PyObject *args)
 		PyErr_SetString(PyExc_Exception, (std::string("enge databasemeneer zei: ")+err).c_str());
 		sqlite3_free(err);
 		if (table) sqlite3_free_table(table);
-		return NULL;
 	}
 	else if (result!=SQLITE_OK)
 	{
+		std::cout << "DB: Error: unknown\n";
 		PyErr_SetString(PyExc_Exception, "boehoe, database lezen is mislukt");
-		return NULL;
 	}
 	else if (ncolumn==0)
 	{
@@ -103,3 +102,64 @@ PyObject * piet_db_query(PyObject *self, PyObject *args)
 	
 	return NULL;
 }
+
+std::string piet_db_get(const std::string &table_, const std::string &key, const std::string &default_)
+{
+	if (!sqlite_t::instance().open())
+		return default_;
+
+	std::string query="SELECT value FROM "+table_+" WHERE key='"+key+"'";
+	std::cout << "DB: query=" << query << "\n";
+	int nrow,ncolumn;
+	char *err=NULL;
+	char **table=NULL;
+	int result=
+		sqlite3_get_table(sqlite_t::instance(), query.c_str(), &table, &nrow, &ncolumn, &err);
+	if (err)
+	{
+		std::cout << "DB: Error: " << err << "\n";
+		sqlite3_free(err);
+		if (table) sqlite3_free_table(table);
+	}
+	else if (result!=SQLITE_OK)
+	{
+		std::cout << "DB: Error: unknown\n";
+	}
+	else if (ncolumn==0)
+	{}
+	else
+	{
+		std::string result=table[1];
+		sqlite3_free_table(table);
+		return result;
+	}
+	
+	return default_;
+}
+
+void piet_db_set(const std::string &table_, const std::string &key_, const std::string &value_)
+{
+	python_lock guard(__PRETTY_FUNCTION__);
+
+	std::string query="REPLACE INTO "+table_+" VALUES('"+key_+"', '"+value_+"')";
+
+	if (!sqlite_t::instance().open()) throw;
+
+	std::cout << "DB: query=" << query << "\n";
+	int nrow,ncolumn;
+	char *err=NULL;
+	char **table=NULL;
+	int result=
+		sqlite3_get_table(sqlite_t::instance(), query.c_str(), &table, &nrow, &ncolumn, &err);
+	if (table) sqlite3_free_table(table);
+	if (err)
+	{
+		std::cout << "DB: Error" << err << "\n";
+		sqlite3_free(err);
+	}
+	else if (result!=SQLITE_OK)
+	{
+		std::cout << "DB: Error: unknown\n";
+	}
+}
+
--- piet_db.h
+++ piet_db.h
@@ -4,8 +4,25 @@
 #define __PIET_DB_H__
 
 #include <Python.h>
+#include <string>
+#include <boost/lexical_cast.hpp>
 
 PyObject * piet_db_query(PyObject *self, PyObject *args);
 
+template<typename T>
+T piet_db_get(const std::string &table_, const std::string &key_, const T default_)
+{
+	return boost::lexical_cast<T>(piet_db_get(table_, key_, boost::lexical_cast<std::string>(default_)));
+}
+
+template<typename T>
+void piet_db_set(const std::string &table_, const std::string &key_, const T value_)
+{
+	return piet_db_set(table_, key_, boost::lexical_cast<std::string>(value_));
+}
+
+
+std::string piet_db_get(const std::string &table_, const std::string &key_, const std::string &default_);
+void piet_db_set(const std::string &table_, const std::string &key_, const std::string &default_);
 
 #endif // __PIET_DB_H__
