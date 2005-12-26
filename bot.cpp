20c270e8c617ac8d2d510c9553248473c65bc6bc
simon <devnull@localhost>
2005-12-26 12:20:23 +0000

    [project @ config zonder lua]

--- bot.cpp
+++ bot.cpp
@@ -1,20 +1,39 @@
 
 
 #include "bot.h"
-#include <lubi.h>
+#include <fstream>
+#include <iostream>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 
 
-c_piet_config::c_piet_config() : _server("irc.xs4all.nl"), _service("ircd"),
-																 _initial_nick("rarepiet"), _channel("#piettest")
+c_piet_config::c_piet_config() :
+	_server("irc.xs4all.nl"), _service("ircd"),
+	_initial_nick("rarepiet"), _channel("#piettest")
 {
 	try
 	{
-		lubi::lua_state state;
-		state.run("dofile(\"piet_config.lua\");");
-		_server=state["server"].get<std::string>();
-		_service=state["service"].get<std::string>();
-		_initial_nick=state["initial_nick"].get<std::string>();
-		_channel=state["channel"].get<std::string>();
+		std::ifstream i("piet.conf");
+		char line[1024];
+		while (i.getline(line, 1024))
+		{
+			char *p=strchr(line, '=');
+			if (p)
+			{
+				std::string key(line, p);
+				std::string value(p+1);
+				boost::algorithm::trim(key);
+				boost::algorithm::trim(value);
+				if (boost::algorithm::iequals(key, "server"))
+					_server=value;
+				else if (boost::algorithm::iequals(key, "service"))
+					_service=value;
+				else if (boost::algorithm::iequals(key, "initial_nick"))
+					_initial_nick=value;
+				else if (boost::algorithm::iequals(key, "channel"))
+					_channel=value;
+			}
+		}
 
 		_nick=_initial_nick;
 	}
