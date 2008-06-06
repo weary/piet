14449a7054862b7bf5b73cd3e101ee2f86fb79e2
weary <weary@weary.nl>
2008-06-06 18:13:08 +0200

    better commandline parsing

--- gps.py
+++ gps.py
@@ -11,6 +11,32 @@ def gps_coord(regel):
   result=get_url(url)
   if string.find(result,"Your search for") > 0:
     return "Staat niet op de kaart"
+  if string.find(result,"Did you mean") > 0:
+    x=string.find(result,"Did you mean")
+    x=string.find(result,"geocode=",x)+8
+    y=string.find(result,"\\",x)
+    geocode=result[x:y]
+    x=string.find(result,"ref_desc",x)
+    x=string.find(result,"x3e",x)+3
+    y=string.find(result,"x3c",x)-1
+    place=result[x:y]
+    geocodes=string.split(geocode,",")
+    if float(geocodes[1])>0:
+      geocode="N: "
+    else:
+      geocode="S: "
+    geocode+=str(abs(int(float(geocodes[1]))))+"°"
+    x=string.find(geocodes[1],".")
+    geocode+=str(float("0."+geocodes[1][x+1:])*60)[:6]+"' "
+    if float(geocodes[2])>0:
+      geocode+="E: "
+    else:
+      geocode+="W: "
+    geocode+=str(abs(int(float(geocodes[2]))))+"°"
+    x=string.find(geocodes[2],".")
+    geocode+=str(float("0."+geocodes[2][x+1:])*60)[:6]+"'"
+    return place+": GPS: "+geocode
+
   x=string.find(result,"{lat:")+5
   y=string.find(result,",",x)
   lat=result[x:y]
@@ -42,5 +68,7 @@ def gps_coord(regel):
   if x<6:
     minutes="0"+minutes
   result+="°"+minutes[:6]+"'"
+  if string.strip(result)=="GPS: N: 37°03.75' W: 95°40.624'":
+    return "Staat niet op de kaart"
   return result
 
